de3048ad6dc9366924744b96f95b63c3918e2ffd
Kennet Postigo <kennetpostigo@Kennets-MacBook-Pro.local>
2015-09-09 09:28:57 -0400

    Error handling.

--- BattleShip/src/battleship/BattleShipGame.java
+++ BattleShip/src/battleship/BattleShipGame.java
@@ -16,14 +16,12 @@ import javax.swing.JOptionPane;
 // Lets user know whether they hit or miss ships.
 //
 //////////////////////////// 80 columns wide/////////////////////////////////
-public class BattleShipGame 
-{
+public class BattleShipGame {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Battleship myGame = new Battleship();// TODO code application logic here
         String input = null;
         String playAgain = null;
@@ -31,68 +29,60 @@ public class BattleShipGame
         int gamesWon = 0;
         int gamesLost = 0;
         String[] checkInput = new String[5];
-        do
-        {
-        JOptionPane.showMessageDialog(null, String.format("             "
-                + "              Welcome to the game of Battleship %n"
-                + "       In this game a 5 by 5 game board will be created with"
-                + " random %n   battleship locations. You will be given four "
-                + "attempts to find them %n                            "
-                + "   If you find "
-                + "all four you win!!!!" + " %n(" + "Please enter your attempts"
-                + " in this form: ex (row,column) = (5,5)" + ")"));
-        
-       myGame.initialize_ship_locations();
-       Arrays.fill(checkInput, null);
-         for(int i = 1; i < 5; i++)
-        {
-           input = JOptionPane.showInputDialog("Guess " + i);
-           
-           if(input == null)
-           {
-               System.out.println("Thanks for playing");
-               break;
-           }
-           else
-           {
-           Scanner myScan = new Scanner(input);
-           while(Arrays.asList(checkInput).contains(input))
-           {
-               input = JOptionPane.showInputDialog("The same numbers have been"
-                       + " previously entered. Please try again");
-           }
-           int k = myScan.useDelimiter(",").nextInt();
-           int j = myScan.useDelimiter(",").nextInt();
-           myGame.process_user_guess(k, j);
-           checkInput[i] = input;
-           }
-        }
-         if(input != null)
-        {
-          myGame.displayBattleshipGame();
-             if (myGame.userWon()) 
-             {
-                System.out.println("\nCongrats, you sunk all 4 ships!!!!");
-                gamesWon++;
-             } 
-             else 
-             {
-                 System.out.println("\nShips Sunk: " +myGame.getTotalShipsSunk() 
-                         + "\nShips Missed: " + myGame.getTotalMisses());
-                 gamesLost++;
-             }
-             
-             System.out.println("Games Won: " + gamesWon + "\nGames Lost: " +
-                     gamesLost);
-          myGame.clear_ship_locations();
-          playAgain = JOptionPane.showInputDialog("Do you want to play again? Y"
-                  + "/N");
-        }
-         else
-         {
-             break;
-         }
-        }
-        while(playAgain.equalsIgnoreCase("Y") );
-}
+        do {
+            JOptionPane.showMessageDialog(null, String.format("             "
+                    + "              Welcome to the game of Battleship %n"
+                    + "       In this game a 5 by 5 game board will be created with"
+                    + " random %n   battleship locations. You will be given four "
+                    + "attempts to find them %n                            "
+                    + "   If you find "
+                    + "all four you win!!!!" + " %n(" + "Please enter your attempts"
+                    + " in this form: ex (row,column) = (5,5)" + ")"));
+
+            myGame.initialize_ship_locations();
+            Arrays.fill(checkInput, null);
+            for (int i = 1; i < 5; i++) {
+                input = JOptionPane.showInputDialog("Guess " + i);
+
+                if (input == null) {
+                    System.out.println("Thanks for playing");
+                    break;
+                } else {
+                    Scanner myScan = new Scanner(input);
+                    
+                    while (Arrays.asList(checkInput).contains(input)) {
+                        input = JOptionPane.showInputDialog("The same numbers have been"
+                                + " previously entered. Please try again");
+                    }
+                    int k = myScan.useDelimiter(",").nextInt();
+                    int j = myScan.useDelimiter(",").nextInt();
+                    myGame.process_user_guess(k, j);
+                    checkInput[i] = input;
+                }
+            }
+            if (input != null) {
+                myGame.displayBattleshipGame();
+                if (myGame.userWon()) {
+                    System.out.println("\nCongrats, you sunk all 4 ships!!!!");
+                    gamesWon++;
+                } else {
+                    System.out.println("\nShips Sunk: " + myGame.getTotalShipsSunk()
+                            + "\nShips Missed: " + myGame.getTotalMisses());
+                    gamesLost++;
+                }
+
+                System.out.println("Games Won: " + gamesWon + "\nGames Lost: "
+                        + gamesLost);
+                myGame.clear_ship_locations();
+                playAgain = JOptionPane.showInputDialog("Do you want to play again? Y"
+                        + "/N");
+                if (playAgain != "y" || playAgain != "yes") {
+                    System.out.println("\nThanks for playing, have a good day!");
+                    break;
+                }
+            } else {
+                break;
+            }
+        } while (playAgain.equalsIgnoreCase("Y") || playAgain.equalsIgnoreCase("Yes"));
+    }
 }
--- BattleShip/src/battleship/Battleship.java
+++ BattleShip/src/battleship/Battleship.java
@@ -2,8 +2,7 @@ package battleship;
 
 import java.util.Random;
 
-public class Battleship 
-{
+public class Battleship {
 
     Random shipSpot = new Random();
     private boolean ship_locations[][] = new boolean[5][5];
@@ -14,28 +13,25 @@ public class Battleship
     /**
      * Initializes a Battleship Object
      */
-    public Battleship() 
-    {}
+    public Battleship() {
+    }
 
     /**
      * Initializes random ship locations.
      */
-    public void initialize_ship_locations() 
-    {
+    public void initialize_ship_locations() {
         int row = shipSpot.nextInt(5) + 0;
         int column = shipSpot.nextInt(5) + 0;
         ship_locations[row][column] = true;
         int temp = row;
         int temp2 = column;
 
-        for (int i = 0; i < 3; i++) 
-        {
+        for (int i = 0; i < 3; i++) {
             row = shipSpot.nextInt(5) + 0;
             column = shipSpot.nextInt(5) + 0;
 
             //prevents the same two numbers from being generated
-            while (ship_locations[temp][temp2] == ship_locations[row][column]) 
-            {
+            while (ship_locations[temp][temp2] == ship_locations[row][column]) {
                 row = shipSpot.nextInt(5) + 0;
                 column = shipSpot.nextInt(5) + 0;
             }
@@ -46,12 +42,9 @@ public class Battleship
     /**
      * Clear Ship locations at the end of the game.
      */
-    public void clear_ship_locations() 
-    {
-        for (int i = 0; i < ship_locations.length; i++) 
-        {
-            for (int k = 0; k < ship_locations[i].length; k++) 
-            {
+    public void clear_ship_locations() {
+        for (int i = 0; i < ship_locations.length; i++) {
+            for (int k = 0; k < ship_locations[i].length; k++) {
                 ship_locations[i][k] = false;
                 guess_locations[i][k] = false;
             }
@@ -59,7 +52,7 @@ public class Battleship
         totalShipsSunk = 0;
         totalMisses = 0;
     }
-    
+
     /**
      * Processes user input into their guess display and increments hits and
      * misses.
@@ -67,50 +60,40 @@ public class Battleship
      * @param row sets the inputted row
      * @param col sets the inputted col
      */
-    public void process_user_guess(int row, int col) 
-    {
+    public void process_user_guess(int row, int col) {
         guess_locations[row - 1][col - 1] = true;
 
         if (guess_locations[row - 1][col - 1]
-                == ship_locations[row - 1][col - 1]) 
-        {
+                == ship_locations[row - 1][col - 1]) {
             totalShipsSunk++;
         }
         totalMisses = 4 - totalShipsSunk;
     }
-    
+
     /**
      * Checks if user won the battleship match.
      *
      * @return Boolean(true or false)
      */
-    public boolean userWon() 
-    {
-        if (totalShipsSunk == 4) 
-        {
+    public boolean userWon() {
+        if (totalShipsSunk == 4) {
             return true;
         }
 
         return false;
     }
-    
+
     /**
      * Prints the user and random ship game board and total misses and hits they
      * have
      */
-    public void displayBattleshipGame() 
-    {
+    public void displayBattleshipGame() {
         System.out.println("\n\t   User Guesses");
-        for (int i = 0; i < guess_locations.length; i++) 
-        {
-            for (int k = 0; k < guess_locations[i].length; k++) 
-            {
-                if (guess_locations[i][k] == true) 
-                {
+        for (int i = 0; i < guess_locations.length; i++) {
+            for (int k = 0; k < guess_locations[i].length; k++) {
+                if (guess_locations[i][k] == true) {
                     System.out.print("\u2715" + " \t");
-                } 
-                else 
-                {
+                } else {
                     System.out.print("\u25CF" + "\t");
                 }
             }
@@ -118,16 +101,11 @@ public class Battleship
         }
 
         System.out.println("\n\t   Game Board");
-        for (int i = 0; i < ship_locations.length; i++) 
-        {
-            for (int k = 0; k < ship_locations[i].length; k++) 
-            {
-                if (ship_locations[i][k] == true) 
-                {
+        for (int i = 0; i < ship_locations.length; i++) {
+            for (int k = 0; k < ship_locations[i].length; k++) {
+                if (ship_locations[i][k] == true) {
                     System.out.print("\u2715" + " \t");
-                } 
-                else 
-                {
+                } else {
                     System.out.print("\u25CF" + "\t");
                 }
             }
@@ -140,8 +118,7 @@ public class Battleship
      *
      * @return total ships sunk
      */
-    public int getTotalShipsSunk() 
-    {
+    public int getTotalShipsSunk() {
         return totalShipsSunk;
     }
 
@@ -150,8 +127,7 @@ public class Battleship
      *
      * @return total ships missed
      */
-    public int getTotalMisses() 
-    {
+    public int getTotalMisses() {
         return totalMisses;
     }
 }
